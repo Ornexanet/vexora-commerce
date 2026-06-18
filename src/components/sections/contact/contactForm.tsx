32ef454d0e86a6298c9082f0de967040c0b074aa
Ornexanet <ornexawork@gmail.com>
2026-06-18 19:10:37 +0200

    Translate conatct page to Swedish

--- src/components/sections/contact/contactForm.tsx
+++ src/components/sections/contact/contactForm.tsx
@@ -8,37 +8,38 @@ const ContactForm = () => {
   return (
     <div className="bg-[#F5F5F7] rounded-[15px] xl:px-12 lg:px-8 md:px-6 px-4 py-12.5">
       <Title size="60" className="font-bold leading-[130%]">
-        Let’s Talk
+        Kontakta oss
       </Title>
       <p className="mt-5 text-xl leading-[180%] text-light-dark">
-        We’re here to help—reach out with any questions or feedback.
+        Har du frågor eller synpunkter? Kontakta oss så hjälper vi dig gärna.
+
       </p>
       <form className="space-y-5 mt-15">
         <div className="grid xl:grid-cols-2 gap-x-4 gap-y-5">
           <Input
-            placeholder="Your Name"
+            placeholder="Ditt namn"
             className="h-15 bg-background border-l-light-gray px-5 text-lg rounded-lg placeholder:text-light-dark"
           />
           <Input
             type="email"
-            placeholder="Your Email"
+            placeholder="Din e-postadress"
             className="h-15 bg-background border-l-light-gray px-5 text-lg rounded-lg placeholder:text-light-dark"
           />
         </div>
         <div>
           <Input
-            type="number"
-            placeholder="Your Number"
+            type="Telefonnummer"
+            placeholder="Din telefonnummer"
             className="h-15 bg-background border-l-light-gray px-5 text-lg rounded-lg placeholder:text-light-dark"
           />
         </div>
         <div>
           <Textarea
-            placeholder="Write Massage"
+            placeholder="Skriv ditt meddelande"
             className="min-h-[148px] bg-background border-l-light-gray px-5 text-lg rounded-lg placeholder:text-light-dark"
           />
         </div>
-        <Button className="mt-5">Send Massage</Button>
+        <Button className="mt-5">Skicka meddelande</Button>
       </form>
     </div>
   );
