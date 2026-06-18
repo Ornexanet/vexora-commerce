bf8b1de4dc9888d89b938250027b3a11365fd4a5
Ornexanet <ornexawork@gmail.com>
2026-06-18 16:18:41 +0200

    Update compare page

--- src/app/(innerPage)/compare/page.tsx
+++ src/app/(innerPage)/compare/page.tsx
@@ -4,14 +4,50 @@ import PageHeader from "@/components/sections/pageHeader";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Vexora - Compare",
-  description:
-    "Vexora - Single Product eCommerce Next.js Template for Tech and Gadget Stores",
+  title: "Jämför produkter | Ornexa",
+  description:   
+"Jämför produkter på Ornexa och se skillnader i pris, tillgänglighet och viktiga produktdetaljer.",
+alternates: {
+  canonical: "https://shop.ornexa.net/compare",
+},
+robots: {
+  index: false,
+  follow: true,
+},
 };
 
+
 const Compare = () => {
+  const breadcrumbSchema = {
+  "@context": "https://schema.org",
+  "@type": "BreadcrumbList",
+  itemListElement: [
+    {
+      "@type": "ListItem",
+      position: 1,
+      name: "Hem",
+      item: "https://shop.ornexa.net",
+    },
+    {
+      "@type": "ListItem",
+      position: 2,
+      name: "Jämför produkter",
+      item: "https://shop.ornexa.net/compare",
+    },
+  ],
+};
+
   return (
     <main>
+      <script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify(breadcrumbSchema),
+  }}
+/>
+
+
+
       <PageHeader title="Compare" currentPageName="Compare" />
       <CompareTable />
     </main>
