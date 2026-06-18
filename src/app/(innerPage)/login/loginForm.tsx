776455501b0d97725283c65d1f4015cda91a3976
Ornexanet <ornexawork@gmail.com>
2026-06-18 18:31:34 +0200

    Translate login page to Swedish

--- src/app/(innerPage)/login/loginForm.tsx
+++ src/app/(innerPage)/login/loginForm.tsx
@@ -10,33 +10,33 @@ function LoginForm() {
   return (
     <form className="min-w-[360px] mx-auto border border-[#F1F1F4] rounded-xl p-10 space-y-7.5 max-w-[480px] shadow-[0px_3px_4px_0px_#00000008]">
       <div className="text-center">
-        <h6 className="font-medium text-[22px] mb-2.5">Sign in</h6>
+        <h6 className="font-medium text-[22px] mb-2.5">Login</h6>
         <p className="text-light-dark">
-          Need an account?{" "}
+          Har du inget konto?{" "}
           <Link
             href={"/register"}
             className="text-blue hover:underline underline-offset-2"
           >
-            Sign up
+            Skapa konto
           </Link>
         </p>
       </div>
       <div className="flex items-center gap-2.5">
         <button className="border border-[#DBDFE9] rounded-md flex items-center justify-center gap-1 h-11 w-full text-light-dark font-medium hover:border-blue hover:text-blue transition-all duration-300">
-          <GoogleOriginalIcon /> <span>Use Google</span>
+          <GoogleOriginalIcon /> <span>Förtsätt med Google</span>
         </button>
         <button className="border border-[#DBDFE9] rounded-md flex items-center justify-center gap-1 h-11 w-full text-light-dark font-medium hover:border-blue hover:text-blue transition-all duration-300">
-          <AppleOriginalIcon /> <span>Use Apple</span>
+          <AppleOriginalIcon /> <span>Förtsätt med Apple</span>
         </button>
       </div>
       <div className="flex items-center justify-center gap-2">
         <span className="w-full h-px bg-light-gray block"></span>
-        <span className="text-light-dark">OR</span>
+        <span className="text-light-dark">Eller</span>
         <span className="w-full h-px bg-light-gray block"></span>
       </div>
       <div>
         <label htmlFor="email" className="text-lg leading-[14px] mb-2 block">
-          Email
+          E-post
         </label>
         <Input
           placeholder="email@email.com"
@@ -48,18 +48,18 @@ function LoginForm() {
       <div>
         <div className="flex items-center justify-between mb-2">
           <label htmlFor="password" className="text-lg leading-[14px] block">
-            Password
+            Lösenord
           </label>
           <Link
             href={"#"}
             className="text-blue text-sm leading-[14px] hover:underline"
           >
-            Forgot Password?
+            Glömt lösenordet?
           </Link>
         </div>
         <div className="relative">
           <Input
-            placeholder="Enter Password"
+            placeholder="Ange ditt lösenord"
             type="password"
             id="password"
             className="bg-[#FCFCFC] placeholder:text-light-dark"
@@ -75,11 +75,11 @@ function LoginForm() {
       <div className="flex items-center gap-1.5">
         <Checkbox id="remember" className="size-5 rounded-sm [&_svg]:size-4" />
         <label htmlFor="remember" className="text-lg">
-          Remember me
+          Kom ihåg mig
         </label>
       </div>
       <Button type="submit" className="w-full">
-        Sign in
+        Logga in
       </Button>
     </form>
   );
