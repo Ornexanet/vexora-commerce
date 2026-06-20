83e673950a2e9d84a3f68b2ae5d61750d0d3848e
Ornexanet <ornexawork@gmail.com>
2026-06-20 12:44:55 +0200

    Update page

--- src/contextApi/providers.tsx
+++ src/contextApi/providers.tsx
@@ -2,11 +2,15 @@
 
 import React, { memo } from "react";
 import { CartProvider } from "./cartContext";
+import { WishlistProvider } from "./wishlistContext";
+import { CompareProvider } from "./compareContext";
 
 export const Providers = memo(({ children }: { children: React.ReactNode }) => {
   return (
     <CartProvider>
-     {children}
+      <WishlistProvider>
+        <CompareProvider>{children}</CompareProvider>
+      </WishlistProvider>
     </CartProvider>
   );
 });
