b54bbb2f080eb8677aa3b9f5fe7a825133273a63
Ornexanet <ornexawork@gmail.com>
2026-06-20 12:41:29 +0200

    Update providers.tsx

--- src/contextApi/providers.tsx
+++ src/contextApi/providers.tsx
@@ -2,15 +2,11 @@
 
 import React, { memo } from "react";
 import { CartProvider } from "./cartContext";
-import { WishlistProvider } from "./wishlistContext";
-import { CompareProvider } from "./compareContext";
 
 export const Providers = memo(({ children }: { children: React.ReactNode }) => {
   return (
     <CartProvider>
-      <WishlistProvider>
-        <CompareProvider>{children}</CompareProvider>
-      </WishlistProvider>
+     {children}
     </CartProvider>
   );
 });
