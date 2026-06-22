78f5d2de7e6a217f87baa6d72a221f3441a13e92
Ornexanet <ornexawork@gmail.com>
2026-06-22 23:04:55 +0200

    Update

--- src/components/sections/hero/heroSlider.tsx
+++ src/components/sections/hero/heroSlider.tsx
@@ -125,7 +125,8 @@ const HeroSlider = () => {
             <iframe
               width="100%"
               height="100%"
-              src={`https://www.youtube.com/embed/TKnufs85hXk?autoplay=1`}
+              src={isOpen?
+                `https://www.youtube.com/embed/TKnufs85hXk?autoplay=1` : ""}
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
