388b8f9f2da101f87ab5d04bd9901cd2178a5bac
Ornexanet <ornexawork@gmail.com>
2026-07-03 20:45:46 +0200

    Update

--- src/components/header/topHeader.tsx
+++ src/components/header/topHeader.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import {
   InputGroup,
   InputGroupAddon,
@@ -24,26 +25,37 @@ import CartSidebar from "./cartSidebar";
 
 const TopHeader = () => {
   const [isShowSearch, setIsShowSearch] = useState(false);
+
   return (
-    <div className="flex justify-between items-center gap-[50px] relative">
-      <Link href={"/"} className="shrink-0">
-        <Image width={120} height={40} src={"/images/ornexa-logo121.png"} alt="logo" />
+    <div className="grid grid-cols-[auto_1fr_auto] items-center gap-[30px] relative w-full">
+      <Link href={"/"} className="shrink-0 w-[120px] block">
+        <Image
+          width={120}
+          height={40}
+          src={"/images/ornexa-logo121.png"}
+          alt="Ornexa logo"
+          priority
+        />
       </Link>
-      {/* center search input field and  category dropdown */}
+
       <div
         className={cn(
-          "flex-7 max-lg:absolute max-lg:bg-background max-lg:w-full ",
-          isShowSearch ? "block lg:block" : "lg:block hidden",
+          "min-w-0 max-lg:absolute max-lg:left-0 max-lg:top-full max-lg:z-50 max-lg:bg-background max-lg:w-full",
+          isShowSearch ? "block lg:block" : "lg:block hidden"
         )}
       >
         <div className="border border-light-gray rounded-[47px] flex items-center lg:pl-[25px] max-h-[50px] overflow-hidden">
-          <div className="border-r border-r-light-gray py-1.5 h-full">
+          <div className="border-r border-r-light-gray py-1.5 h-full shrink-0">
             <Select defaultValue="All Categories">
               <SelectTrigger className="lg:w-[190px] border-none font-bold lg:text-base text-sm lg:pr-[37px] focus-visible:ring-0 h-auto max-lg:[&>svg]:size-5">
                 <SelectValue />
               </SelectTrigger>
+
               <SelectContent className="min-w-[220px]">
-                <SelectItem value={"All Categories"}>Alla kategorier</SelectItem>
+                <SelectItem value={"All Categories"}>
+                  Alla kategorier
+                </SelectItem>
+
                 {categoriesList.map(({ id, label }) => (
                   <SelectItem key={id} value={label}>
                     {label}
@@ -52,16 +64,19 @@ const TopHeader = () => {
               </SelectContent>
             </Select>
           </div>
+
           <InputGroup className="h-auto border-none rounded-[47px] [&_svg]:size-5 lg:[&_svg]:size-6 lg:pl-[25px] has-[[data-slot=input-group-control]:focus-visible]:ring-0">
             <InputGroupInput
               placeholder="Sök efter produkter..."
               className="py-[15px] h-auto rounded-[47px] placeholder:text-[#B0B5BB] text-[#B0B5BB] font-medium"
             />
-            <InputGroupAddon align={"inline-end"} className="text-foreground ">
+
+            <InputGroupAddon align={"inline-end"} className="text-foreground">
               <InputGroupButton className="lg:mr-3 max-lg:px-0!">
                 <MicroPhone className="size-6" />
               </InputGroupButton>
             </InputGroupAddon>
+
             <InputGroupAddon
               align="inline-end"
               className="text-foreground max-lg:block hidden"
@@ -76,22 +91,22 @@ const TopHeader = () => {
           </InputGroup>
         </div>
       </div>
-      {/* right side countray, user, cart icons and dropdown */}
-      <div className="flex-2 flex items-center justify-end gap-[25px]">
-        {/* search icons for meduim devices */}
+
+      <div className="flex items-center justify-end gap-[25px] shrink-0">
         <div
           onClick={() => setIsShowSearch(true)}
           className="lg:hidden flex items-center gap-2 cursor-pointer"
         >
           <Search size={20} />
           <span className="text-lg leading-normal font-medium lg:block hidden">
-            Search
+            Sök
           </span>
         </div>
-        {/* search icons for meduim devices */}
+
         <div className="xl:flex gap-[25px] hidden">
           <HeaderExtraInfo />
         </div>
+
         <CartSidebar />
       </div>
     </div>
