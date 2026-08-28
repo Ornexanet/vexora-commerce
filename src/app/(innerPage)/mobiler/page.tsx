835bc4e24b27e99fdb60596c2d377011afd5a47c
Ornexanet <ornexawork@gmail.com>
2026-08-28 20:57:11 +0200

    Update mobile page

--- src/app/(innerPage)/mobiler/page.tsx
+++ src/app/(innerPage)/mobiler/page.tsx
@@ -43,6 +43,9 @@ const ShopWithSidebar = () => {
                 <div className="mb-4 xl:hidden">
                   <ProductsFilterSidebarOffacnvise />
                 </div>
+                <h2 className="text-2xl font-bold mb-6">
+  Utforska våra mobiler
+</h2>
                 <ProductsSortHeader />
                 <ProductsContainer />
               </div>
